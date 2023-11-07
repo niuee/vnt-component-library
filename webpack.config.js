ac520e7afc6f7b684a4b4d8d04e65369a18d4070
niuee <vntchang@gmail.com>
2023-11-07 18:27:59 +0800

    Add the webworker script entry point in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: "development",
-  entry: './src/playground/playground.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'devDist'),
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+module.exports = [
+  {
+    mode: "development",
+    entry: { index:'./src/playground/playground.ts', physicsWorker:'./src/workerscripts/physicsWorker.ts'},
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'devDist'),
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        chunks: ['index'],
+        template: './src/playground/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/playground/index.html',
-    }),
-  ],
-  devServer: {
-    static: path.resolve(__dirname, 'devDist'), // Specify the directory for serving static files
-  },
-};
+    devServer: {
+      static: path.resolve(__dirname, 'devDist'), // Specify the directory for serving static files
+    },
+  }
+];
